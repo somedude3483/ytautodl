38e7527b000543544119d99bc5efe70655714ed2
somedude3483 <62268652+somedude3483@users.noreply.github.com>
2020-06-27 19:31:04 -0400

    edit 2
    ping with errors if there are any

--- ytautodl.py
+++ ytautodl.py
@@ -1,31 +1,32 @@
-#!/usr/bin/env python
-# Guide for getting details from youtube videos for future reference.
-# https://developers.google.com/youtube/v3/guides/implementation/videos
-# TODO: Allow users to see which videos have been downloaded.
-# TODO: Allow users to play video from terminal. Possibly use mplayer?
-# TODO: Allow users to set what time task is scheduled. Unsure how to implement this. Would prefer crossplatform, so crontab may be out of the picture.
-# TODO: Allow users to download videos from search.
-# TODO: Show thumbnails in search.
-# TODO: Export data
-# TODO: Allow user to specify arguments for youtubel-dl, for example whether or not to use aria2.
+"""
+Guide for getting details from youtube videos for future reference.
+ https://developers.google.com/youtube/v3/guides/implementation/videos
+ TODO: Allow users to see which videos have been downloaded.
+ TODO: Allow users to play video from terminal. Possibly use mplayer?
+ TODO: Allow users to set what time task is scheduled. Unsure how to implement this. Would prefer crossplatform, so crontab may be out of the picture.
+ TODO: Allow users to download videos from search.
+ TODO: Show thumbnails in search.
+ TODO: Export data
+ TODO: Allow user to specify arguments for youtubel-dl, for example whether or not to use aria2.
 
-# CHANGELOG: Sorta? idk? This is is what I am going to keep track of what has been changed.
-# 1.) Created new function to parse channel rss feeds instead of calling the youtube api.
-# 2.) It should be illegal to use this many ansi escape codes. I'll have to clean this up later.
-# 3.) Decreased visible clutter while running the program.
-# 4.) Started using aria2 to increase speeds of downloads.
-# 5.) Updated database with description column, and downloaded column. Queries marked as downloaded will not be returned from query_db()
-# 6.) Added mark_downloaded query. This will reduce queried results returned, and number of requests to youtube.
-# 7.) Added kickass ascii art.
+ CHANGELOG: Sorta? idk? This is is what I am going to keep track of what has been changed.
+ 1.) Created new function to parse channel rss feeds instead of calling the youtube api.
+ 2.) It should be illegal to use this many ansi escape codes. I'll have to clean this up later.
+ 3.) Decreased visible clutter while running the program.
+ 4.) Started using aria2 to increase speeds of downloads.
+ 5.) Updated database with description column, and downloaded column. Queries marked as downloaded will not be returned from query_db()
+ 6.) Added mark_downloaded query. This will reduce queried results returned, and number of requests to youtube.
+ 7.) Added kickass ascii art.
+"""
 
 
+import os
+import argparse
 import requests
 import subprocess
-import argparse
-from apikey import apikey
-from validation import *
-import os
 import feedparser
+from validation import *
+from apikey import apikey
 
 matrixgreen = "\u001b[38;5;46m"
 removecolor = "\u001b[0m"
@@ -34,10 +35,21 @@ underline = "\u001b[4m"
 
 def main():
 	parser = argparse.ArgumentParser(description="Download Youtube Videos Automatically")
-	parser.add_argument("-new", action='store_true', help="Add a youtube creator to the database.")
-	parser.add_argument("-d", action='store_true', help="Checks database and downloads videos automatically")
-	parser.add_argument("-channels", action='store_true', help="Shows you all channels that have been added to the database.")
-	parser.add_argument("-search", action='store_true', help="Search youtube")
+	parser.add_argument("-new", 
+			    action='store_true', 
+			    help="Add a youtube creator to the database.")
+	
+	parser.add_argument("-d", 
+			    action='store_true', 
+			    help="Checks database and downloads videos automatically")
+	
+	parser.add_argument("-channels", 
+			    action='store_true', 
+			    help="Shows you all channels that have been added to the database.")
+	parser.add_argument("-search", 
+			    action='store_true', 
+			    help="Search youtube")
+	
 	args = parser.parse_args()
 
 	if args.new:
@@ -55,10 +67,10 @@ def main():
 		uploads = query_db()
 		uid_set = set()
 
-		for i in uploads:
-			uid_set.add(i[1])
-		for i in uid_set:
-			parse_ytrss(i)
+		for upload in uploads:
+			uid_set.add(upload[1])
+		for item in uid_set:
+			parse_ytrss(item)
 
 		download_video()
 
@@ -72,10 +84,11 @@ def main():
 
 		for i in data:
 			channel_set.add(i[0])
-		for i in channel_set:
-			print(f"{matrixgreen} {i} {removecolor}\n\n")
+		print([f"{matrixgreen} {i} {removecolor}\n\n" for i in channel_set])
 	else:
-		print(f"{matrixgreen} You did not specify arguments. Type ytautodl.py -h for more information on how to run this program\n{removecolor}")
+		print(f"{matrixgreen} You did not specify arguments. ",
+		      f"Type ytautodl.py -h for more information on how to run this program\n{removecolor}",
+		     sep="\n")
 
 	#TODO: Create argument that will allow you to add from CSV.
 	#TODO: Show related after videos are done downloading.
@@ -91,38 +104,50 @@ def request_uploads(playlist_id):
 
 	link = "https://www.googleapis.com/youtube/v3/playlistItems"
 	r = requests.get(link, params=params)
-	response = r.json()["items"]
-	for i in response:
-		channel_title = i["snippet"]["channelTitle"]
-		channel_id = i["snippet"]["channelId"]
-		uploads_id = i["snippet"]["playlistId"]
-		video_id = i["snippet"]["resourceId"]["videoId"]
-		video_date = i["snippet"]["publishedAt"][0:10]
-		video_title = i["snippet"]["title"]
-		insert_data(channel_title, channel_id, uploads_id, video_id, video_date, video_title)
+	responses = r.json()["items"]
+	for response in responses:
+		channel_title = response["snippet"]["channelTitle"]
+		channel_id = response["snippet"]["channelId"]
+		uploads_id = response["snippet"]["playlistId"]
+		video_id = response["snippet"]["resourceId"]["videoId"]
+		video_date = response["snippet"]["publishedAt"][0:10]
+		video_title = response["snippet"]["title"]
+		
+		insert_data(channel_title, 
+			    channel_id, 
+			    uploads_id, 
+			    video_id, 
+			    video_date, 
+			    video_title)
 
 def parse_ytrss(rss_link):
 	# New function will parse youtube rss feeds to reduce api requests.
 	#TODO: Skip videos which have been marked as downloaded.
 	rss_link = f"https://www.youtube.com/feeds/videos.xml?channel_id={rss_link}"
 
-
-	r = requests.get(rss_link)
-	page = r.text
+	resp = requests.get(rss_link)
+	page = resp.text
 
 	d = feedparser.parse(page)
 	entries = d.entries
 
-	for i in entries:
-		channel_title = i['authors'][0]['name'] 
-		channel_id = i['yt_channelid'] 
+	for entry in entries:
+		channel_title = entry['authors'][0]['name'] 
+		channel_id = entry['yt_channelid'] 
 		uploads_id = "UU" + channel_id[2::]
-		video_id = i['yt_videoid'] 
-		video_date = i['published'][0:10]
-		video_title = i['title'] 
-		description = i['summary']
+		video_id = entry['yt_videoid'] 
+		video_date = entry['published'][0:10]
+		video_title = entry['title'] 
+		description = entry['summary']
 		downloaded = '0'
-		insert_data(channel_title, channel_id, uploads_id, video_id, video_date, video_title, description, downloaded)
+		insert_data(channel_title,
+			    channel_id,
+			    uploads_id,
+			    video_id,
+			    video_date,
+			    video_title,
+			    description,
+			    downloaded)
 
 
 def search_results():
@@ -142,22 +167,23 @@ def search_results():
 	count = 0
 	for i in response["items"]:
 		count = count + 1
-		channelTitle = i["snippet"]["channelTitle"]
-		videoTitle = i["snippet"]["title"]
+		channel_title = i["snippet"]["channelTitle"]
+		video_title = i["snippet"]["title"]
 		description = i["snippet"]["description"]
-		publishTime = i["snippet"]["publishTime"]
+		publish_time = i["snippet"]["publishTime"]
 		try:
 			video_id = i["id"]["videoId"]
 			videos.append(video_id)
-			print('\n\n')
-			print(linesep.replace("*", str(count)))
-			print("| Channel:", channelTitle)
-			print("| Video:", videoTitle)
+			print(f"\n\n{linesep.replace("*", str(count))}",
+			     f"| Channel: {channel_title}",
+			     f"| Video: {video_title}",
+			     sep="\n")
+			
 			if description == "":
 				print("| Description: None")
 			else:
 				print("| Description:", description)
-			print("| Uploaded:", publishTime)
+			print("| Uploaded:", publish_time)
 			print(linesep)
 		except KeyError:
 			count -= 1
@@ -185,9 +211,9 @@ def download_video():
 		video_title = i[5]
 		path = os.getcwd() + "/" + channel_title
 		download = ytprefix + video_id
-		if os.path.isdir(path) == False:
+		if not os.path.isdir(path):
 				os.mkdir(path)
-				print ("Directory %s created" % path)
+				print (f"Directory {path} created")
 		clearline = "\033[K"
 		moveup = "\033[1A"
 		movedown = "\033[1B"
@@ -204,6 +230,7 @@ def download_video():
 		sys.stdout.write(clearline)
 		fetch = subprocess.run(["youtube-dl", "--no-warnings", "-o", "%(uploader)s - %(title)s.%(ext)s", "--external-downloader", "aria2c", "--external-downloader-args", "'-x 8 -s 8 -k 1m'", download], stdout=subprocess.DEVNULL)
 		os.chdir("..")
+		print(fetch.returncode)
 		if fetch.returncode == 0:
 			mark_downloaded(video_id)
 
